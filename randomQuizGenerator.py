e3e6950b1ee08fd93ca2da7a4976f298a6a0d0fb
alkuczino <26746416+alkuczino@users.noreply.github.com>
2023-08-21 02:02:59 +0200

    stuff

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -11,12 +11,41 @@ import ast
 #def saveQuiz():
     ###TODO
 ###
-#def generateQuiz():
-    ###TODO
 
-#def generateQuestion(answers):
-    ###TODO
+def generateQuiz(capitals, quizes_number):
+    for quiz_number in quizes_number:
+        generateQuizfile(quiz_number)
+        for question_number in len(capitals):
+            generateQuestion(capitals, question_number)
+            appendAnswerfile(quiz_number, answer)
+        
+def generateQuizfile(quiz_number):
+    with open (f'capitals_quiz{quiz_number + 1}.txt', 'a') as quiz_file:
+        quiz_file.write('Name:\n\nDate:\n\n:Group:\n\n')
+        quiz_file.write((' ' * 20) + f'State capitals Quiz (Form{quiz_number + 1})\n\n')
 
+def appendQuizFile(capitals, quiz_number, question):
+    with open (f'capitals_quiz{quiz_number + 1}.txt', 'a') as quiz_file:
+        quiz_file.write(f'What is the capital of {capitals}')
+
+def appendAnswerfile(quiz_number, answer):
+    with open(f'capitals_quiz_answers{quiz_number +1}.txt', 'a') as answers_file:
+        answers_file.write(f'{quiz_number}. {answer}\n')
+        
+        
+def generateQuestion(capitals, question_number):
+    states = list(capitals.keys()) 
+    correct_answer = capitals[states[question_number]]
+    wrong_answers = list(capitals.values())
+    del wrong_answers[wrong_answers.index(correct_answer)]
+    wrong_answers = random.sample(wrong_answers, 3)
+    answer_options = wrong_answers + [correct_answer]
+    random.shuffle(answer_options)
+    question = f'What is the capital of {capitals.key(correct_answer)}?\n'
+    return answer_options
+
+    
+    
 def loadAnswers(file_path):
     with open(file_path) as input_file:
         lines = input_file.readlines()
@@ -28,4 +57,5 @@ def loadAnswers(file_path):
 
 if __name__ == '__main__':
     answers = loadAnswers(Path.cwd() / 'capitals.txt')
-    print(answers)
+    #print(answers)
+    print(generateQuestion(answers, 0))
