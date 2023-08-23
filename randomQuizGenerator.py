8c3e190c7ff8d0cf6ee0170937ce73423e960b25
alkuczino <26746416+alkuczino@users.noreply.github.com>
2023-08-23 02:49:53 +0200

    Final

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -4,47 +4,50 @@
    
 import random
 from pathlib import Path
-import os
-import pprint
 import ast 
 
-#def saveQuiz():
-    ###TODO
-###
 
-def generateQuiz(capitals, quizes_number):
-    for quiz_number in quizes_number:
-        generateQuizfile(quiz_number)
-        for question_number in len(capitals):
-            generateQuestion(capitals, question_number)
-            appendAnswerfile(quiz_number, answer)
+def generateQuiz(input_capitals, quizes_number):
+    for quiz_number in range(quizes_number):
+        generateQuizFile(quiz_number)
+        generateAnswersFile(quiz_number)
+        capitals = {k: input_capitals[k] for k in random.sample(list(input_capitals.keys()), len(input_capitals))}
+        question_number = 1
+        for state, capital in capitals.items():
+            answers = generateAnswers(capitals, capital)
+            str_answers = '\n'.join(['. '.join(text) for text in answers])
+            answer = '\n'.join(['. '.join(text[0]) for text in answers if capital in text])
+            appendQuizFile(quiz_number, f'{question_number}. What is the capital of {state}?\n\n{str_answers}\n\n')
+            appendAnswersFile(quiz_number, f'{question_number}. {answer}\n')
+            question_number += 1
         
-def generateQuizfile(quiz_number):
+def generateQuizFile(quiz_number):
+    with open (f'capitals_quiz{quiz_number + 1}.txt', 'w') as quiz_file:
+        quiz_file.write('Name:\n\nDate:\n\nGroup:\n\n')
+        quiz_file.write((' ' * 20) + f'State capitals Quiz (Form {quiz_number + 1})\n\n')
+        
+def generateAnswersFile(quiz_number):
+    with open(f'capitals_quiz_answers{quiz_number + 1}.txt', 'w') as answers_file:
+        answers_file.write(f'Answers for quiz number {quiz_number + 1}\n')
+        
+def appendQuizFile(quiz_number, text):
     with open (f'capitals_quiz{quiz_number + 1}.txt', 'a') as quiz_file:
-        quiz_file.write('Name:\n\nDate:\n\n:Group:\n\n')
-        quiz_file.write((' ' * 20) + f'State capitals Quiz (Form{quiz_number + 1})\n\n')
+        quiz_file.write(f'{text}')
 
-def appendQuizFile(capitals, quiz_number, question):
-    with open (f'capitals_quiz{quiz_number + 1}.txt', 'a') as quiz_file:
-        quiz_file.write(f'What is the capital of {capitals}')
-
-def appendAnswerfile(quiz_number, answer):
-    with open(f'capitals_quiz_answers{quiz_number +1}.txt', 'a') as answers_file:
-        answers_file.write(f'{quiz_number}. {answer}\n')
+def appendAnswersFile(quiz_number, text):
+    with open(f'capitals_quiz_answers{quiz_number + 1}.txt', 'a') as answers_file:
+        answers_file.write(f'{text}')
         
-        
-def generateQuestion(capitals, question_number):
-    states = list(capitals.keys()) 
-    correct_answer = capitals[states[question_number]]
-    wrong_answers = list(capitals.values())
-    del wrong_answers[wrong_answers.index(correct_answer)]
-    wrong_answers = random.sample(wrong_answers, 3)
-    answer_options = wrong_answers + [correct_answer]
-    random.shuffle(answer_options)
-    question = f'What is the capital of {capitals.key(correct_answer)}?\n'
-    return answer_options
+def generateAnswers(capitals, correct_answer):
+    letters = ['A', 'B', 'C', 'D']
+    all_answers = list(capitals.values())
+    del all_answers[all_answers.index(correct_answer)]
+    all_answers = random.sample(all_answers, 3)
+    all_answers.append(correct_answer)
+    random.shuffle(all_answers)
+    answers = [(letter, answer) for (letter, answer) in zip(letters, all_answers)]
+    return answers
 
-    
     
 def loadAnswers(file_path):
     with open(file_path) as input_file:
@@ -56,6 +59,5 @@ def loadAnswers(file_path):
 
 
 if __name__ == '__main__':
-    answers = loadAnswers(Path.cwd() / 'capitals.txt')
-    #print(answers)
-    print(generateQuestion(answers, 0))
+    input_capitals = loadAnswers(Path.cwd() / 'capitals.txt')
+    generateQuiz(input_capitals, 35)
